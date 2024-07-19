0e63b45926cafa95226537bccc2a62d94f565f39
abc <abc@gmail.com>
2024-07-19 13:47:03 +0530

    ddsdsds

---
+++ src/main/java/mca/Hello.java
@@ -0,0 +1,14 @@
+package mca;
+
+public class Hello {
+	
+	
+
+	 public String palindrome(String s)
+	    {
+	    StringBuffer s1=new StringBuffer(s);
+	    String str=s1.reverse().toString();
+	    return str;
+	    }
+
+}
